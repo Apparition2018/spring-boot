0884a54f3f04dbff8792b410517334557e0375a6
Apparition2018 <88850180@163.com>
2022-04-07 02:30:59 +0800

    2022-04-07 02:30:00

---
+++ thymeleaf/Thymeleaf.md
@@ -0,0 +1,8 @@
+# Thymeleaf
+
+---
+## Reference
+1. [Thymeleaf](https://www.thymeleaf.org)
+2. [Thymeleaf 教程](https://fanlychie.github.io/post/thymeleaf.html)
+3. [Thymeleaf 视频教程](https://www.bilibili.com/video/BV1cq4y1E79S)
+---
---
+++ thymeleaf/src/test/java/com/ljh/ThymeleafTests.java
@@ -0,0 +1,60 @@
+package com.ljh;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+/**
+ * ThymeleafTests
+ *
+ * @author ljh
+ * created on 2022/4/7 1:01
+ */
+public class ThymeleafTests {
+
+    TemplateEngine engine;
+    Context context;
+
+    @BeforeEach
+    public void init() {
+        engine = new TemplateEngine();
+        context = new Context();
+    }
+
+    /**
+     * 模板字符串
+     */
+    @Test
+    public void testTemplateString() {
+        String template = "<template type='text' th:value='hello_thymeleaf'/>";
+        String html = engine.process(template, context);
+        System.out.println("结果：" + html);
+    }
+
+    /**
+     * 变量
+     */
+    @Test
+    public void testVariable() {
+        String template = "<template type='text' th:value='${name}'/>";
+        context.setVariable("name", "张三");
+        String html = engine.process(template, context);
+        System.out.println("结果：" + html);
+    }
+
+    /**
+     * 模板文件
+     */
+    @Test
+    public void testTemplateFile() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        engine.setTemplateResolver(resolver);
+        context.setVariable("name", "张三");
+        String html = engine.process("index", context);
+        System.out.println("结果：" + html);
+    }
+}
