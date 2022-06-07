4705c3d0de339aa9e09448c0a71433d26a827c8b
Apparition2018 <88850180@163.com>
2022-06-08 01:40:27 +0800

    2022-06-08 01:40:00

---
+++ activiti/Activiti.md
@@ -0,0 +1,21 @@
+# Activiti
+
+---
+## Reference
+1. [Activiti](https://www.activiti.org/)
+2. [Activiti/Activiti](https://github.com/Activiti/Activiti)
+3. [Activiti 最全讲解_哔哩哔哩](https://www.bilibili.com/video/BV1t64y147v4)
+---
+## 术语
+- BPM：Business Process Management，业务流程管理
+- BPMN：Business Process Model and Notation，业务流程模型和符号
+---
+## 使用步骤
+1. 部署 Activiti
+2. 定义流程
+3. 部署流程
+4. 启动流程实例
+5. 查询代办任务
+6. 办理任务
+7. 流程结束
+---
---
+++ activiti/src/main/java/com/ljh/ActivitiApplication.java
@@ -0,0 +1,18 @@
+package com.ljh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ActivitiApplication
+ *
+ * @author ljh
+ * created on 2022/6/8 1:18
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+}
