a716c8d794124039b674fe79b857103b479c057f
Apparition2018 <88850180@163.com>
2022-08-26 16:55:09 +0800

    2022-08-26 16:55:00

--- spring-boot-common/src/main/java/com/ljh/mapper/SysUserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ljh.mapper;
-
-import com.ljh.pojo.SysUser;
-import com.ljh.util.MyMapper;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SysUserMapper extends MyMapper<SysUser> {
-}
